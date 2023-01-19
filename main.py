9781d5ecd19615617d544af487fa1b1542a5cffc
Víctor Gomes <joaogomespcl@gmail.com>
2023-01-18 22:31:42 -0300

    Manage Files

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from shutil import move
+
+# Define the "path" variable as the user's downloads folder
+path = os.path.expanduser("~/Downloads")
+
+# Start a loop to go through all files in the downloads folder
+for file in os.listdir(path):
+    # Check if the current file is actually a file and not a folder
+    if os.path.isfile(os.path.join(path, file)):
+        # Get the extension of the current file
+        extension = file.split(".")[-1]
+        # Create the path for the folder with the current file's extension
+        extension_path = os.path.join(path, extension)
+        # Check if the folder with the current file's extension already exists. If it doesn't, create a new folder with the extension's name
+        if not os.path.exists(extension_path):
+            os.mkdir(extension_path)
+        # Move the current file to the folder with its corresponding extension
+        move(os.path.join(path, file), os.path.join(extension_path, file))
+
+
+
+
