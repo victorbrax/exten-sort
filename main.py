9bcd5f582eb71a041d1b080155ebcf349eb11075
Víctor Gomes <joaogomespcl@gmail.com>
2023-05-15 00:32:45 -0300

    fix no-extensions files

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ path = os.path.expanduser("~/Downloads")
 
 # Start a loop to go through all files in the downloads folder
 for file in os.listdir(path):
-    # Check if the current file is actually a file and not a folder
+    # Check if the current item is a file and not a folder
     if os.path.isfile(os.path.join(path, file)):
-        # Get the extension of the current file
-        extension = file.split(".")[-1]
+        # Check if the file has an extension
+        if '.' in file:
+            # Get the extension of the current file
+            extension = file.split(".")[-1]
+        else:
+            # Skip the file if it doesn't have an extension
+            continue
+
         # Create the path for the folder with the current file's extension
         extension_path = os.path.join(path, extension)
+
         # Check if the folder with the current file's extension already exists. If it doesn't, create a new folder with the extension's name
         if not os.path.exists(extension_path):
             os.mkdir(extension_path)
+
         # Move the current file to the folder with its corresponding extension
         move(os.path.join(path, file), os.path.join(extension_path, file))
-
-
-
-
